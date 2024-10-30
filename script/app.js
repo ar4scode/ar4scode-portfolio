745e10b5b7fb450ae3dcc1854c83d9145b9afd68
Ar4sCode <ar4sazizi@gmail.com>
2024-10-30 07:23:43 +0330

    Create about section below home section and modify the links

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 function toggleActive(button) {
   // Remove "active" class from all buttons
-  const buttons = document.querySelectorAll(".nav-btns");
+  const buttons = document.querySelectorAll(".nav-links");
   buttons.forEach((btn) => btn.classList.remove("active"));
 
   // Add "active" class to the clicked button
