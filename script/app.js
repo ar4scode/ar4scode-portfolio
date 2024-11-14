26351cb3beaebfc5220075c957ff48f288cb5c71
Ar4sCode <ar4sazizi@gmail.com>
2024-11-14 13:24:21 +0330

    Modify Hire me button to show my phone and social links

--- script/app.js
+++ script/app.js
@@ -6,3 +6,16 @@ function toggleActive(button) {
   // Add "active" class to the clicked button
   button.classList.add("active");
 }
+
+// Create window when we click on hire button
+const hireMePopup = document.querySelector(".pop-up");
+
+document.querySelector(".hire-me-btn").addEventListener("click", () => {
+  hireMePopup.classList.add("pop-up-active");
+  hireMePopup.classList.remove("pop-up-not-active");
+});
+
+document.querySelector(".exit-btn").addEventListener("click", () => {
+  hireMePopup.classList.add("pop-up-not-active");
+  hireMePopup.classList.add("pop-up-active");
+});
